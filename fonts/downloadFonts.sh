1846809574ae748fd9f4c3962bb1c16de3060e82
StringManolo <manuelvarelacaldas@gmail.com>
2026-01-08 00:41:57 +0000

    Updates

---
+++ fonts/downloadFonts.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Oxygen Regular (400)
+curl -o oxygen-regular.woff2 "https://fonts.gstatic.com/s/oxygen/v16/2sDfZG1Wl4LcnbuKjk0m.woff2"
+
+# Oxygen Bold (700)
+curl -o oxygen-700.woff2 "https://fonts.gstatic.com/s/oxygen/v16/2sDcZG1Wl4LcnbuCNWgzaGW5.woff2"
+
+# Verify
+ls -lh
